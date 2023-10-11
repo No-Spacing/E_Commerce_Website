919be76e776556a81999e7429801871d641d3eb5
No-Spacing <64142238+No-Spacing@users.noreply.github.com>
2023-10-11 23:28:24 +0800

    fixed footer set to bottom part

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <style>
-    .footer {
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        z-index: 999;
-
-    }
+html {
+  position: relative;
+  min-height: 100%;
+  padding-bottom:160px;
+}
+body {
+  margin-bottom: 160px;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 150px;
+}
 </style>
 <html>
     <head>
@@ -25,10 +31,16 @@
             @include('layouts.login')
         @endif
         @yield('content')
-        <div class="footer footer-copyright">
-            <div class="d-flex flex-row bg-dark pt-3 container-fluid">
-                <div class="col-md-12 text-center ">
-                    <p style="color: white;">Copyright Brigada Healthline Corp. © 2022. All rights reserved.</p>
+        <div class="footer">
+            <div class="d-flex flex-row bg-dark pt-3 container-fluid ">
+                <div class="col-md-12 container">
+                    <div class="col col-xl-2">
+                        <h4 style="color: white;">Daet Branch</h4>
+                        <p style="color: white;">Kenboy Bldg, Central Plaza Complex, Brgy Lag-on, Daet Camarines Norte.</p>
+                    </div>
+                    <div class=" d-flex justify-content-center">
+                        <p style="color: white;">COPYRIGHT © {{ date('Y') }} Brigada Healthline Corp.</p>
+                    </div>   
                 </div>
             </div>
         </div>
@@ -39,6 +51,14 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+<script>
+    var botmanWidget = {
+        aboutText: 'about',
+        introMessage: "✋ Hi! I'm Bot from Brigada Healthline Corp. How may I assist you?"
+    };
+</script>
+  
+<script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
 @if($errors->get('emailLogin') || $errors->get('passwordLogin') || Session::get('fail'))
     <script>
         $(document).ready(function() {             
