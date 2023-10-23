505ee41414aaf5d75d9ea87c95ec2388d82a4b22
No-Spacing <64142238+No-Spacing@users.noreply.github.com>
2023-10-23 18:59:38 +0800

    Update profile.blade.php

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -2,7 +2,7 @@
 	<div class="modal-dialog modal-dialog-centered modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">				
-				<h1 class="modal-title text-primary">Edit Profile</h1>
+				<h5>Edit Profile</h5>
 				<!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button> -->
