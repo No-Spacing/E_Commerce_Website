7556dc365ad49d4f726d74312c7c800513db0f77
No-Spacing <64142238+No-Spacing@users.noreply.github.com>
2023-10-25 16:31:49 +0800

    refresh on navigate

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -101,6 +101,18 @@
        
     </div>
 </div>
-    
+<script>
+    window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+                            ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+        // Handle page restore.
+        window.location.reload();
+    }
+    });
+</script>
+
+
 @stop
 
--- resources/views/view-product.blade.php
+++ resources/views/view-product.blade.php
@@ -135,6 +135,16 @@ function scrollToBottom()
     var height = document.body.scrollHeight;
     window.scroll(0 , height);
 }
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted || 
+                         ( typeof window.performance != "undefined" && 
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
+
 </script>
 
 @stop
