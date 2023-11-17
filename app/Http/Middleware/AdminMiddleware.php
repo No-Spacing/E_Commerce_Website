9b99d2e1917327b519f9b6839b42b596b0ef5053
No-Spacing <64142238+No-Spacing@users.noreply.github.com>
2023-11-18 05:07:46 +0800

    Update AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -20,11 +20,11 @@ class AdminMiddleware
         }
 
         if(session()->has('Admin') && ($request->path() == 'adminLogin')){
-            return back();
+            return redirect()->route('admin.home');
         }
 
-        return $next($request)  ->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
-                                ->header('Pragma','no-cache')
-                                ->header('Expires','Sat 01 Jan 1990 00:00:00 GMT');
+        return $next($request)->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
+                            ->header('Pragma','no-cache')
+                            ->header('Expires','Sat 01 Jan 1990 00:00:00 GMT');
     }
 }
