90a97e12d9b0c47e131959d0ff404973d1369418
No-Spacing <64142238+No-Spacing@users.noreply.github.com>
2023-11-25 18:43:33 +0800

    Update salesPdf.blade.php
    Layouting SALES PDF

--- resources/views/salesPdf.blade.php
+++ resources/views/salesPdf.blade.php
@@ -1,1 +1,20 @@
-PDF
+<html>
+  <body>
+  <h2><img src="img/brigada.jpg"></h2>
+  <p class="dateTab">{{ date('Y-m-d') }}</p>
+    <table style="width:100%">
+      <tr>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Total Sold</th>
+      </tr>
+      @foreach($sales as $sale)
+        <tr>
+            <td>{{ $sale->product }}</td>
+            <td>{{ $sale->Position }}</td>
+            <td>{{ $sale->quantity }} Item(s)</td>
+        </tr>
+      @endforeach
+    </table>
+  </body>
+</html>
