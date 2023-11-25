6856c2c8f10d77eb6fc0d821e6c2348103daf8a9
No-Spacing <64142238+No-Spacing@users.noreply.github.com>
2023-11-25 18:57:50 +0800

    Update salesPdf.blade.php
    add data to sales pdf

--- resources/views/salesPdf.blade.php
+++ resources/views/salesPdf.blade.php
@@ -1,18 +1,22 @@
 <html>
   <body>
-  <h2><img src="img/brigada.jpg"></h2>
+  <h2><img src="img/brigada-cover.png"></h2>
   <p class="dateTab">{{ date('Y-m-d') }}</p>
     <table style="width:100%">
       <tr>
         <th>Name</th>
-        <th>Position</th>
+        <th>Item Cost</th>
+        <th>Item Price</th>
+        <th>Profit Per Item</th>
         <th>Total Sold</th>
       </tr>
       @foreach($sales as $sale)
         <tr>
-            <td>{{ $sale->product }}</td>
-            <td>{{ $sale->Position }}</td>
-            <td>{{ $sale->quantity }} Item(s)</td>
+            <td>{{ $sale->product_name }}</td>
+            <td>{{ $sale->item_cost }}</td>
+            <td>{{ $sale->item_price }}</td>
+            <td>{{ $sale->item_cost - $sale->item_price }}</td>
+            <td>{{ $sale->total_sold }}</td>
         </tr>
       @endforeach
     </table>
